0cda1d073e8a2a02198eab3346787fb3af9c9b82
Zarema Khalilova <zarema.khalilova@gmail.com>
2017-02-13 22:23:30 +0300

    Add uploadcare-widget and ImageUpload component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import ImageUpload from './ImageUpload.jsx';
 
 export default class App extends React.Component {
   render() {
     return (
       <div id="content">
         <h5>Time to <a href="https://facebook.github.io/react/">React</a>.</h5>
+        <ImageUpload/>
       </div>
     );
   }
